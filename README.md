0e353cad2fd916f54c0fd00d416b9bb82c0537c3
totocalcio <totohyper@yahoo.co.jp>
2023-08-06 17:35:03 +0900

    add prisma

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - Tailwind
 - Pinia
 - Nuxt Image
+- Prisma
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,3 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
-  colorMode: {
-    preference: 'acid',
-    dataValue: 'theme',
-    classSuffix: '',
-  },
 })
